7425bb21f64a804a147608f42b20aea094071e86
Lokananda Dhage Munisamappa <lokananda@Lokanandas-MacBook-Pro.local>
2016-10-31 17:02:13 -0500

    fix

--- PageRank/src/PageRankCustomPartitioning.py
+++ PageRank/src/PageRankCustomPartitioning.py
@@ -5,17 +5,29 @@ Created on Oct 29, 2016
 @author: lokananda
 '''
 #
-# Report the application completion time under the three different scenarios.
-# Compute the amount of network/storage read/write bandwidth used during the application lifetime under the four different scenarios.
-# Compute the number of tasks for every execution.
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
 #
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""
+"""
 
 
 import re
 import sys
 from operator import add
-
-from pyspark.sql import SparkSession
 from pyspark import SparkConf, SparkContext
 
 def computeContribs(urls, rank):
@@ -41,6 +53,7 @@ if __name__ == "__main__":
           file=sys.stderr)
 
     sparkConf = SparkConf().setAppName("CS-838-Assignment2-PartA-1")\
+    .setMaster("spark://10.254.0.160:7077")\
     .set("spark.executor.memory", "1G")\
     .set("spark.eventLog.enabled", "true")\
     .set("spark.evenLog.dir", "file:///tmp/test")\
@@ -68,7 +81,7 @@ if __name__ == "__main__":
     #lines = spark.read.text(sys.argv[1]).rdd.map(lambda r: r[0])
 
     # Loads all URLs from input file and initialize their neighbors.
-    links = lines.map(lambda urls: parseNeighbors(urls)).distinct().groupByKey().cache()
+    links = lines.map(lambda urls: parseNeighbors(urls)).distinct().groupByKey()
 
     # Loads all URLs with other URL(s) link to from input file and initialize ranks of them to one.
     ranks = links.map(lambda url_neighbors: (url_neighbors[0], 1.0))
@@ -85,5 +98,4 @@ if __name__ == "__main__":
     # Collects all URL ranks and dump them to console.
     for (link, rank) in ranks.collect():
         print("%s has rank: %s." % (link, rank))
-
     sc.stop()
