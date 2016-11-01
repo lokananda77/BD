92720b135704246a3506c330b7e2cf85d107fadc
Lokananda Dhage Munisamappa <lokananda@Lokanandas-MacBook-Pro.local>
2016-11-01 16:57:42 -0500

    read from csv

--- Streaming/src/Streaming.py
+++ Streaming/src/Streaming.py
@@ -6,9 +6,10 @@ Created on Oct 31, 2016
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import explode
 from pyspark.sql.functions import split
+from pyspark.sql.types import StructType 
 
 spark = SparkSession\
-    .builder()\
+    .builder\
     .appName("StructuredNetworkWordCount")\
     .getOrCreate()
     
@@ -21,20 +22,29 @@ lines = spark\
    .option('port', 9999)\
    .load()
 
+
+userSchema = StructType().add("name", "string").add("age", "integer")
+csvDF = spark \
+    .readStream() \
+    .option("sep", ";") \
+    .schema(userSchema) \
+    .csv("/split_dataset_monitored") 
+    
+    
 # Split the lines into words
 words = lines.select(
    explode(
        split(lines.value, ' ')
    ).alias('word')
 )
-
+ 
 # Generate running word count
 wordCounts = words.groupBy('word').count()
-
+ 
 query = wordCounts\
     .writeStream\
     .outputMode('complete')\
     .format('console')\
     .start()
-
-query.awaitTermination()
+# 
+# query.awaitTermination()
